af24972b45e2b1b4bb8c0e6679683a06d33c5ae7
Shrikant Shinde <110349806+shrikantshinde178@users.noreply.github.com>
2024-01-01 15:13:52 +0530

    Minimize the Maximum of Two Arrays : Solution

--- Day_01/MinimizeSet.java
+++ Day_01/MinimizeSet.java
@@ -1,4 +1,4 @@
-// Problem Statement : 
+// Problem Statement: 
 // We have two arrays arr1 and arr2 which are initially empty. You need to add positive integers to them such that they satisfy all the following conditions:
 // arr1 contains uniqueCnt1 distinct positive integers, each of which is not divisible by divisor1.
 // arr2 contains uniqueCnt2 distinct positive integers, each of which is not divisible by divisor2.
