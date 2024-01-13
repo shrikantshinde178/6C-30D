dfe314eba549dd3731035a8576e0774cdce74bab
Shrikant Shinde <110349806+shrikantshinde178@users.noreply.github.com>
2024-01-13 22:30:15 +0530

    Create EqualSubstring.java

---
+++ Day_13/EqualSubstring.java
@@ -0,0 +1,49 @@
+// Problem Statement:
+// You are given two strings s and t of the same length and an integer maxCost.
+
+// You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
+
+// Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+
+// Example 1:
+
+// Input: s = "abcd", t = "bcdf", maxCost = 3
+// Output: 3
+// Explanation: "abc" of s can change to "bcd".
+// That costs 3, so the maximum length is 3.
+// Example 2:
+
+// Input: s = "abcd", t = "cdef", maxCost = 3
+// Output: 1
+// Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+// Example 3:
+
+// Input: s = "abcd", t = "acde", maxCost = 0
+// Output: 1
+// Explanation: You cannot make any change, so the maximum length is 1.
+ 
+// Constraints:
+
+// 1 <= s.length <= 105
+// t.length == s.length
+// 0 <= maxCost <= 106
+// s and t consist of only lowercase English letters.
+
+// Solution:
+class EqualSubstring {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int left = 0, right, ans = 0, curr = 0, n = s.length();
+
+        for(right = 0; right < n; right++) {
+            curr += Math.abs(s.charAt(right) - t.charAt(right));
+
+            while(left <= right && curr > maxCost) {
+                curr -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
